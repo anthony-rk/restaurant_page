504f07265938aeaff736c5d168dbf9bd4bcb2f18
Anthony Kowalski <anthonyrkowalski@gmail.com>
2020-04-24 16:34:33 -0700

    Add event listeners to Tab buttons

--- src/page_initializer.js
+++ src/page_initializer.js
@@ -1,10 +1,10 @@
 // initializes the content on the page
+import pizzaJPG from './all-meat-pizza.jpg';
+import { showPanel } from "./tabs_builder";
 
 let initializer = function() {
-
     function component(newElement, newInnerHTML) {
         const element = document.createElement(newElement);
-
         element.innerHTML = newInnerHTML;
 
         return element;
@@ -18,11 +18,29 @@ let initializer = function() {
     contentDiv.appendChild(component("h2", "Our pizza made daily from scratch!"));
 
     // Add the Pizza img
-    const pizzaImg = document.createElement("img");
-    pizzaImg.src = "assets/all-meat-pizza.jpg";
+    const pizzaImg = new Image();
+    pizzaImg.src = pizzaJPG;
     contentDiv.appendChild(pizzaImg);
 
-}
+    // Add click event listener for the Tab Buttons
+    let addOnClickFnToTabButtons = function() {
+        document.getElementById('btn-1').addEventListener('click', function() {
+            showPanel(0, '#f44336');
+        });
+        document.getElementById('btn-2').addEventListener('click', function() {
+            showPanel(1, '#4caf50');
+        });
+        document.getElementById('btn-3').addEventListener('click', function() {
+            showPanel(2, '#2196f3');
+        });
+        document.getElementById('btn-4').addEventListener('click', function() {
+            showPanel(3, '#ff5722');
+        });
+    }();
+    showPanel(0, '#f44336');
+
+
+};
 
 export {
     initializer
